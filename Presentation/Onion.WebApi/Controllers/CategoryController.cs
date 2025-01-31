0341252d1b2eda0d0d84be57d7dd5543affdeee3
emredaglar <emre.daglar1@gmail.com>
2025-01-31 10:50:44 +0300

    #

--- Presentation/Onion.WebApi/Controllers/CategoryController.cs
+++ Presentation/Onion.WebApi/Controllers/CategoryController.cs
@@ -14,7 +14,7 @@ namespace Onion.WebApi.Controllers
         private readonly RemoveCategoryCommandHandler _removeCommandHandler;
         private readonly GetCategoryByIdQueryHandler _getCategoryByIdQueryHandler;
         private readonly GetCategoryQueryHandler _getCategoryQueryHandler;
-              
+                
         public CategoryController(CreateCategoryCommandHandler createCommandHandler, UpdateCategoryCommandHandler updateCommandHandler, RemoveCategoryCommandHandler removeCommandHandler, GetCategoryByIdQueryHandler getCategoryByIdQueryHandler, GetCategoryQueryHandler getCategoryQueryHandler)
         {
             _createCommandHandler = createCommandHandler;
